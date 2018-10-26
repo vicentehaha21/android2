0cbf9f3e2e4039dc3be31de020696b2bf4b5582c
Lucas <lucas.oliveira@zup.com.br>
2018-10-26 14:59:45 -0300

    removing androidjunit runner

--- app/src/test/java/com/example/lucas/todoapplication/UselessUnitTest.java
+++ app/src/test/java/com/example/lucas/todoapplication/UselessUnitTest.java
@@ -1,17 +1,13 @@
 package com.example.lucas.todoapplication;
 
-import android.support.test.runner.AndroidJUnit4;
-
 import org.junit.Assert;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
-@RunWith(AndroidJUnit4.class)
 public class UselessUnitTest {
 
     @Test
